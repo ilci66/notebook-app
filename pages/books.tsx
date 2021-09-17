71f93e723247be326565f07eea62ff565027b03b
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-17 11:14:50 +0300

    propswith children error fix

--- pages/books.tsx
+++ pages/books.tsx
@@ -1,25 +1,48 @@
 import { NextPage } from "next";
 
+
+// working on this to get rid of book.name doesn't exist error
+interface AllBook {
+    name: string,
+    description: string,
+    notes: string
+}
+interface AllBooks extends Array<AllBook>{}
+
 export const getStaticProps = async () => {
     const res = await fetch('http://localhost:3000/api/books');
     const data = await res.json();
-    // console.log(data)
+    const books: AllBooks = data.books
     return {
-        props: { books: data.books }
+        props: { books: books }
     }
 }
 
+
 // wrote this to get rid of the the type any error in props
 interface Props {
-    books: object[]
+    books:[
+        {
+            name: string,
+            description: string,
+            notes: string
+        }  
+    ]
 }
 
+// const Books: NextPage<Props> = (props) => {
 const Books: NextPage<Props> = (props) => {
-    const { books } = props;
+    // props.books: object[]
+    const { books } = props
     console.log(books)
     return(
         <div> 
             <h1>The books I read</h1>
+            <ul>
+            {books.map(book => (
+                <li key={book.name}>{book.name}</li>
+            ))}
+            </ul>
         </div>
     )
 }
