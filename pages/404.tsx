a9d4de2f0819f33531cde744243c93e6c9723300
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-17 21:16:48 +0300

    custom 404 page created

---
+++ pages/404.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { useEffect } from 'react';
+// this one caused error about the usaage of hooks
+// import { useRouter as router }  from 'next/router';
+import { useRouter }  from 'next/router';
+import { NextPage } from 'next';
+import styles from '../styles/Home.module.css'
+
+const NotFound: NextPage = () => {
+    const router = useRouter()
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+        }, 5000)
+    }, []);
+
+    return(
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                This is not the page you are looking for
+            </h1>
+            <h3 className={styles.title}>Obi-Wan Kenobi</h3>
+            <p className={styles.description}>
+                Don't worry young one you shall be redirected
+            </p>
+        </div>
+    )
+}
+
+
+export default NotFound
