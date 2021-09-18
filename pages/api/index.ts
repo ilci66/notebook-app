663cee79eed27780673940b9e640580a2d03467f
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-18 15:31:06 +0300

    api/index page created

---
+++ pages/api/index.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+// gonna establish database connection on this page instead 
+// of doing it on every api call again and again
+
+type Data = {
+  message: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ message: 'Stuff will be indexed in this page' })
+}
